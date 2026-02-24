c379b7b41eaec30a581eb1836b3624d60396a915
MBL IT Manager <mblitmanager@gmail.com>
2026-02-24 13:25:20 +0300

    fix: Update cwd path in ecosystem configuration for backend

--- projet-app/backend/ecosystem.config.js
+++ projet-app/backend/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "aopia-backend",
       script: "dist/main.js",
-      cwd: "/var/www/projet-app/backend",
+      cwd: "/var/www/analyse/projet-app/backend",
       env: {
         NODE_ENV: "production",
         PORT: 3001
