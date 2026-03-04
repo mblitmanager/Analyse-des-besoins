72845d9c65d665bdf326642e31e153a9624ef677
MBL IT Manager <mblitmanager@gmail.com>
2026-03-04 14:09:21 +0300

    chore: install backend project dependencies.

--- projet-app/frontend-react/src/components/ui/Card.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function Card({ children, className }) {
-  return <div className={`p-4 bg-white rounded-lg shadow-sm ${className || ''}`}>{children}</div>
-}
