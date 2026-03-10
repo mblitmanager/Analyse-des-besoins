502b8a08afdf127eede55757c504a74ff05b1e8a
MBL IT Manager <mblitmanager@gmail.com>
2026-03-10 17:02:16 +0300

    feat: Add backend and frontend environment configurations and new Playwright test files.

---
+++ tests/Anglais/A1-KO.spec.ts
@@ -0,0 +1,54 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+  await page.getByRole("textbox", { name: "Nom", exact: true }).click();
+  await page.getByRole("textbox", { name: "Nom", exact: true }).fill("df");
+  await page.getByRole("textbox", { name: "Nom", exact: true }).press("Tab");
+  await page.getByRole("textbox", { name: "Prénom" }).fill("f");
+  await page.getByRole("textbox", { name: "Prénom" }).press("Tab");
+  await page.getByRole("textbox", { name: "Téléphone" }).fill("a");
+  await page.getByRole("button", { name: "Démarrer le parcours" }).click();
+  await page
+    .getByRole("textbox", { name: "Ex: Assistant administratif," })
+    .click();
+  await page
+    .getByRole("textbox", { name: "Ex: Assistant administratif," })
+    .fill("aa");
+  await page.locator(".formation-card__radio").first().click();
+  await page.getByText("Occasionnellement").first().click();
+  await page.locator("label").filter({ hasText: "Oui" }).first().click();
+  await page.locator("label").filter({ hasText: "Oui" }).nth(2).click();
+  await page
+    .locator("label")
+    .filter({ hasText: "Quotidiennement" })
+    .nth(1)
+    .click();
+  await page
+    .locator("div:nth-child(5) > .grid > label > .option-card__radio")
+    .first()
+    .click();
+  await page.locator("label").filter({ hasText: /^Oui$/ }).nth(3).click();
+  await page.getByText("Occasionnellement").nth(2).click();
+  await page.getByRole("button", { name: "Valider mon profil" }).click();
+  await page.getByRole("button", { name: "spellcheck Anglais" }).click();
+  await page.getByRole("button", { name: "Continuer arrow_forward" }).click();
+  await page.locator("label").filter({ hasText: "Lycée" }).click();
+  await page.locator("label").nth(5).click();
+  await page.locator("label").filter({ hasText: "Non" }).nth(1).click();
+  await page.getByRole("button", { name: "Continuer arrow_forward" }).click();
+  await page.locator('label:has-text("is")').first().click();//non séléctionné
+  await page
+    .locator(
+      "div:nth-child(2) > .p-6 > .grid > label:nth-child(2) > .option-card__radio",
+    )
+    .click();
+  await page.locator("label").filter({ hasText: "is" }).nth(3).click();
+  await page.getByText("is", { exact: true }).nth(2).click();
+  await page.locator('label').filter({ hasText: /^watching$/ }).click();
+  await page.locator("label").filter({ hasText: "goes" }).click();
+  await page.getByRole("button", { name: "Suivant arrow_forward" }).click();
+  
+  // Dernière action
+  
+});
