925970f5b6e5b54f5dcfd062f13e66961ab4ba0e
MBL IT Manager <mblitmanager@gmail.com>
2026-03-10 16:19:29 +0300

    feat: Add initial Playwright tests, generate test reports, and include frontend and backend environment configuration files.

--- tests/sketchup/Sketchup.spec.ts
+++ tests/sketchup/Sketchup.spec.ts
@@ -1,14 +1,13 @@
 import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
-  // Recording...
   await page.goto("http://localhost:5173/");
   await page.getByRole("textbox", { name: "Nom", exact: true }).click();
-  await page.getByRole("textbox", { name: "Nom", exact: true }).fill("IIA");
+  await page.getByRole("textbox", { name: "Nom", exact: true }).fill("ICDL");
   await page.getByRole("textbox", { name: "Nom", exact: true }).press("Tab");
-  await page.getByRole("textbox", { name: "Prénom" }).fill("INkrea");
+  await page.getByRole("textbox", { name: "Prénom" }).fill("Sketchup");
   await page.getByRole("textbox", { name: "Prénom" }).press("Tab");
-  await page.getByRole("textbox", { name: "Téléphone" }).fill("a");
+  await page.getByRole("textbox", { name: "Téléphone" }).fill("06");
   await page.getByRole("textbox", { name: "Téléphone" }).press("Tab");
   await page
     .getByRole("textbox", { name: "Conseiller commercial (" })
@@ -23,24 +22,18 @@ test("test", async ({ page }) => {
   await page
     .getByRole("textbox", { name: "Ex: Assistant administratif," })
     .fill("AF");
-  // await page.locator(".formation-card__radio").first().click();
+  await page.locator(".formation-card__radio").first().click();
+  await page.getByText("Quotidiennement").first().click();
+  await page.getByText("Oui avec quelques difficultés").first().click();
   await page
-    .locator("label")
-    .filter({ hasText: "person_outlineSalarié" })
-    .click();
-  await page.getByText("Occasionnellement").first().click();
-  await page.locator("label").filter({ hasText: "Non" }).first().click();
-  await page.locator("label").filter({ hasText: "Non" }).nth(1).click();
-  await page
-    .locator(
-      "div:nth-child(4) > .grid > label:nth-child(2) > .option-card__radio",
-    )
+    .locator("div:nth-child(3) > .grid > label > .option-card__radio")
+    .first()
     .click();
   await page
-    .locator(
-      "div:nth-child(5) > .grid > label:nth-child(2) > .option-card__radio",
-    )
+    .locator("div:nth-child(4) > .grid > label > .option-card__radio")
+    .first()
     .click();
+  await page.locator("label").filter({ hasText: "Oui" }).nth(4).click();
   await page.locator("label").filter({ hasText: "Non" }).nth(3).click();
   await page
     .locator("label")
@@ -48,32 +41,22 @@ test("test", async ({ page }) => {
     .nth(2)
     .click();
   await page.getByRole("button", { name: "Valider mon profil" }).click();
-  await page.getByRole("button", { name: "Ignorer et continuer" }).click();
-  await page.getByRole("button", { name: "star Intelligence" }).click();
+  await page.getByRole("button", { name: "square Sketchup" }).click();
   await page.getByRole("button", { name: "Continuer arrow_forward" }).click();
-  await page
-    .locator("div")
-    .filter({ hasText: "quizQuel est l’objectif de" })
-    .nth(5)
-    .click();
   await page
     .getByRole("combobox")
     .selectOption(
       "Je me prépare à entrer dans une formation qualifiante ou à un concours d'entrée",
     );
+  await page.getByText("Non").first().click();
   await page.locator("label").nth(2).click();
-  await page.locator(".option-card__radio").first().click();
   await page.getByRole("button", { name: "Continuer arrow_forward" }).click();
-  await page
-    .locator("div")
-    .filter({ hasText: "schedulePour votre formation" })
-    .nth(5)
-    .click();
   await page.getByText("wb_twilightAprès-midi").click();
+  await page.getByRole("textbox").click();
+  await page.getByRole("textbox").fill("fdgdg");
   await page
     .getByRole("button", { name: "Valider mes disponibilités" })
     .click();
-  await page
-    .getByRole("textbox", { name: "Ex: Assistant administratif," })
-    .fill("IA");
+  await page.goto("http://localhost:5173/validation");
+  await page.getByText("Récapitulatif de votre demande").click();
 });
