9d47c06acf3108af799dd65e6b2fb5774c54c0ce
MBL IT Manager <mblitmanager@gmail.com>
2026-06-29 10:42:19 +0300

    a

--- projet-app/backend/src/main.ts
+++ projet-app/backend/src/main.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
